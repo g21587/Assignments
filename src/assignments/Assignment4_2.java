e130ebfdac855f8039fc691e6c271645f3cbe3cd
Grishma Shah <g21587@gmail.com>
2025-05-28 20:16:48 +0530

    Assignment 4 done

--- src/assignments/Assignment4_2.java
+++ src/assignments/Assignment4_2.java
@@ -1,10 +1,32 @@
 package assignments;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Assignment4_2 {
 
 	public static void main(String[] args) {
 		/* Create a set of the top 10 most visited tourist attractions in the world and print out all
 		of them and its size.*/
+		
+		Set<String> attractions = new HashSet<>();
+        attractions.add("Eiffel Tower");
+        attractions.add("Great Wall of China");
+        attractions.add("Statue of Liberty");
+        attractions.add("Taj Mahal");
+        attractions.add("Machu Picchu");
+        attractions.add("Christ the Redeemer");
+        attractions.add("Colosseum");
+        attractions.add("Niagara Falls");
+        attractions.add("Louvre Museum");
+        attractions.add("Burj Khalifa");
+
+        // Print individually
+        System.out.println("Top Tourist Attractions:");
+        System.out.println(attractions.toString());
+
+        // Print size
+        System.out.println("Total attractions: " + attractions.size());
 
 	}
 
--- src/assignments/Assignment4_4.java
+++ src/assignments/Assignment4_4.java
@@ -1,11 +1,23 @@
 package assignments;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Assignment4_4 {
 
 	public static void main(String[] args) {
 		/* Create a list of the top 5 highest-grossing movies of all time and print out the third
 		movie on the list. */
-		
+		  List<String> movies = Arrays.asList(
+		            "Avatar",
+		            "Avengers: Endgame",
+		            "Titanic",
+		            "Star Wars: The Force Awakens",
+		            "Avengers: Infinity War"
+		        );
+
+		        // 3rd movie = index 2
+		        System.out.println("3rd highest-grossing movie: " + movies.get(2));
 		
 
 	}
